04fde0b1992abefc9013b40705a3ef881a90ce67
Unknown <michael.r.hooi@gmail.com>
2018-11-15 17:41:07 -0500

    use train mode

--- collab_compet.py
+++ collab_compet.py
@@ -12,7 +12,7 @@ def ddpg(n_episodes, num_agents=1, slow_every=100, slow_by=None):
     all_scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
     for i_episode in range(1, n_episodes +1):
-        env_info = env.reset(train_mode=False)[brain_name]     # reset the environment    
+        env_info = env.reset(train_mode=(i_episode % 100) != 0)[brain_name]     # reset the environment    
         states = env_info.vector_observations.reshape(-1,)                  # get the current state (for each agent)
         scores = np.zeros(num_agents)                          # initialize the score (for each agent)
         agent.reset()
@@ -55,7 +55,7 @@ def load_agent():
   agent.critic_target.load_state_dict(critic_state_dict)
 
 def watch_one_episode(slow_by=None):
-  env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+  env_info = env.reset(train_mode=False)[brain_name] # reset the environment
   state = env_info.vector_observations[0]            # get the current state
   score = 0
   while True:
